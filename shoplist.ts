757f50d97c21fb159652378807a331378b479753
MetalDogL <lilkavanilka@gmail.com>
2019-02-23 11:23:39 +0100

    Added 2 ways of running Shoplist (array/object)

--- shoplist.ts
+++ shoplist.ts
@@ -1,14 +1,42 @@
 'use strict';
 
+//method1 of doing this with OBJECT using HASOWNPROPERTY()
+
 let shopList = {
-  eggs: 0,
-  milk: 0,
-  fish: 0,
-  apples: 0,
-  bread: 0,
-  chicken: 0
+	eggs: 0,
+	milk: 0,
+	fish: 0,
+	apples: 0,
+	bread: 0,
+	chicken: 0
+};
+
+function stockStatus(): any {
+
+	let items = Object.keys(shopList);
+  
+  console.log(shopList.hasOwnProperty('milk'));
+	console.log(shopList.hasOwnProperty('bananas'));
 }
 
-function showInfo: void ()
-let items = Object.keys(shopList); 
+stockStatus();
 
+
+//method2 of doing this with ARRAY using INCLUDES()
+
+/* let shopList: string[] = [ 'eggs', 'milk', 'fish', 'apples', 'bread', 'chicken' ];
+
+function stockStatus(params: string[]) {
+	if (shopList.includes('milk')) {
+		console.log('yes');
+	} else {
+		console.log('no');
+	}
+	if (shopList.includes('bananas')) {
+		console.log('yes');
+	} else {
+		console.log('no');
+	}
+}
+
+stockStatus(shopList); */
