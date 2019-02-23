b5895d129053ae1f75813b421d4c4c9bf6b4f8be
MetalDogL <lilkavanilka@gmail.com>
2019-02-23 13:36:11 +0100

    Personal finance finished

--- personalexpense.ts
+++ personalexpense.ts
@@ -4,16 +4,25 @@ export {};
 let expenses: number[] = [500, 1000, 1250, 175, 800, 120];
 
 function analyze(params: number[]) {
-  console.log(params.reduce(function(accu: number, current: number) {
+  console.log(params.reduce(function(accu: number, current: number) {     //shows how much was spent in total
   return accu += current}));
-  let max: number = 0;
+
+  let sum: number = expenses.reduce(function(accu, current) {   //finding average expense
+    accu += current;
+    return accu;
+  });
+  
+  console.log(sum / expenses.length);
+
+  let max: number = 0;                  //showing greatest expense
   params.forEach(element => {
     if (max < element) {
       max = element;
     }
   });
   console.log(max);
-  let min: number = expenses[0];
+
+  let min: number = expenses[0];        //showing cheapest expense
   params.forEach(element => {
     if (min > element) {
       min = element;
